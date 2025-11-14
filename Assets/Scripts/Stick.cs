b61e2afb2721c29e719c1db4c6e699d8a0368c37
Zovmateri <oleg199920000@mail.ru>
2025-11-14 18:09:47 +0300

    ClassWork8Committ

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -5,9 +5,14 @@ namespace Golf
 {
     public class Stick : MonoBehaviour
     {
+        [SerializeField] [Min(0)] private float m_power = 250;
+        [SerializeField] private Transform m_point;
         [SerializeField] private float m_minAngleZ = -30;
         [SerializeField] private float m_maxAngleZ = 30;
         [SerializeField][Min(0)] private float m_speed;
+
+        private Vector3 m_lastPointPosition;
+        private Vector3 m_direction;
         private void FixedUpdate()
         {
             var angels = transform.localEulerAngles;
@@ -21,10 +26,22 @@ namespace Golf
                 angels.z = Rotate(angels.z, m_maxAngleZ);
             }
             transform.localEulerAngles = angels;
+
+            m_direction = (m_point.position - m_lastPointPosition).normalized;
+            m_lastPointPosition = m_point.position;
         }
+
         private float Rotate(float angleZ, float target)
         {
             return Mathf.MoveTowardsAngle(angleZ, target, m_speed * Time.deltaTime);
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+            if (collision.gameObject.TryGetComponent<StoneComponent>(out var stone))
+            {
+                stone.GetComponent<Rigidbody>().AddForce(m_power * m_direction, ForceMode.Force);
+            }
         }
     }
 }
